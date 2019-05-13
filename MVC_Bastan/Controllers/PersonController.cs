6e9c1d4afb7996658b6742cfd667d67b91e4a8a6
mesut.alkan <mesut.alkan@BK-LAB204-IS004>
2019-05-13 14:06:04 +0300

    mvc baştan başladık

---
+++ MVC_Bastan/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using MVC_Bastan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Bastan.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            PersonModel model = new PersonModel();
+            model.Name = "Hasan Mesut Alkan";
+            model.BirthDate = new DateTime(1987,11,01);
+
+            model.GsmNumberList = new List<string>();
+            model.GsmNumberList.Add("5553131313");
+            model.GsmNumberList.Add("5302437272");
+
+            model.AddressList = new List<string>();
+            model.AddressList.Add("Bakırköy Bilge Adam Şubesi");
+            model.AddressList.Add("Ataşehir'in bi mahallesi");
+
+            return View(model);
+        }
+    }
+}
---
+++ MVC_Bastan/Models/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Bastan.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public List<string> GsmNumberList { get; set; }
+
+        public List<string> AddressList { get; set; }
+    }
+}
